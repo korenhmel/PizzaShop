ea362d0dd1db0d3976ad30fdd8c9fb5a645ba81c
Koren <koren.hmel@gmail.com>
2016-07-07 22:33:23 +0300

    add Pizza products to table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160707112614) do
+ActiveRecord::Schema.define(version: 20160707173404) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
