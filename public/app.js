f7a1ab4b48aff9eac18661311defe67a757318a3
Koren <koren.hmel@gmail.com>
2016-07-14 14:43:23 +0300

    add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -1,16 +1,49 @@
-function something()
-{
+function something() {
   var y = window.localStorage.getItem('bbb');
-  y = y*1+1; // x = x+1
+  y = y * 1 + 1; // x = x+1
   window.localStorage.setItem('bbb', y);
   alert(y);
 }
 
-function add_to_cart(id)
-{
-  var key = 'product_'+ id
-  var x = window.localStorage.getItem( key);
-  x = x*1+1;
+function add_to_cart(id) {
+  var key = 'product_' + id
+  var x = window.localStorage.getItem(key);
+  x = x * 1 + 1;
   window.localStorage.setItem(key, x);
-  alert(x);
+  alert('Added ' + (cart_get_number_of_items()) + ' pizza(s) in your cart' );
+
+}
+
+
+function cart_get_number_of_items()
+{
+  var cnt = 0;
+  for (var i = 0; i < window.localStorage.length; i++) {
+    var key = window.localStorage.key(i); // получаем ключ
+    var value = window.localStorage.getItem(key); // получаем значение, аналог в ruby hh[key] = x
+    if (key.indexOf('product_') == 0)
+    {
+      // cnt++;
+      cnt = cnt + value*1;
+    }
   }
+  return cnt;
+}
+
+
+
+//   window.localStorage.key(0);
+//   var key = window.localStorage.key(0);
+//   var value =  window.localStorage.getItem(key)
+//   key.indexOf('product_')
+
+
+//var sum = 0;
+//for (var i = 0; i < localStorage.length; i++) {
+//  var key = localStorage.key(i);
+//  sum += Number(localStorage[key]);
+//}
+//
+// console.log(sum);
+//
+
